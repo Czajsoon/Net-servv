5f826d55884c73c201d326a76f44fe2b515ba594
Jakub Czajkowski <44880904+Czajsoon@users.noreply.github.com>
2021-10-16 17:31:08 +0200

    spring boot project init

---
+++ net-servv/src/main/java/netservv/netservv/NetServvApplication.java
@@ -0,0 +1,13 @@
+package netservv.netservv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetServvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetServvApplication.class, args);
+	}
+
+}
---
+++ net-servv/src/test/java/netservv/netservv/NetServvApplicationTests.java
@@ -0,0 +1,13 @@
+package netservv.netservv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetServvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
