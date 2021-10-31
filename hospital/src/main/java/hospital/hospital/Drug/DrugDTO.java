e117bb7588fc9669f685182636b2c3baf56eaab3
Jakub Czajkowski <44880904+Czajsoon@users.noreply.github.com>
2021-10-31 15:29:39 +0100

    Drug rest
    + GET drug Controllers
    + POST drug Controller
    + DELETE drug Controller

---
+++ hospital/src/main/java/hospital/hospital/Drug/DrugDTO.java
@@ -0,0 +1,10 @@
+package hospital.hospital.Drug;
+
+import lombok.Data;
+
+@Data
+public class DrugDTO {
+    private String drug_name;
+    private Integer amount_in_warehouse;
+    private Float price;
+}
---
+++ hospital/src/main/java/hospital/hospital/Drug/DrugRepository.java
@@ -0,0 +1,8 @@
+package hospital.hospital.Drug;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DrugRepository extends JpaRepository<Drug,Long> {
+}
