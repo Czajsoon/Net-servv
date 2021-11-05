9ea388a05049e3a04d2b87b953281bfddaa25d48
Jakub Czajkowski <44880904+Czajsoon@users.noreply.github.com>
2021-11-05 19:33:57 +0100

    drug prezentation

--- hospital/src/main/java/hospital/hospital/Drug/DrugController.java
+++ hospital/src/main/java/hospital/hospital/Drug/DrugController.java
@@ -1,5 +1,6 @@
 package hospital.hospital.Drug;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import java.util.NoSuchElementException;
 @CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/drug")
+@Slf4j
 public class DrugController {
 
     @Autowired
@@ -17,23 +19,29 @@ public class DrugController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Drug> drugGET(@PathVariable Long id){
-        try { return ResponseEntity.ok().body(drugRepository.getById(id));}
+        try {
+            log.info("GET: " + drugRepository.getById(id));
+            return ResponseEntity.ok().body(drugRepository.getById(id));
+        }
         catch (NoSuchElementException ex){return ResponseEntity.ok().body(null);}
     }
 
     @GetMapping
     public ResponseEntity<List<Drug>> drugGET(){
+        log.info("GET: " + drugRepository.findAll());
         return ResponseEntity.ok().body(drugRepository.findAll());
     }
 
     @PostMapping
-    public ResponseEntity<String> drugPOST(@RequestBody DrugDTO dto){
+    public ResponseEntity drugPOST(@RequestBody DrugDTO dto){
         drugRepository.save(Drug.of(dto));
+        log.info("POST: " + dto);
         return ResponseEntity.ok().body("Drug successfully uploaded!");
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<List<Drug>> drugDELETE(@PathVariable Long id){
+        log.info("DELETE: " + drugRepository.getById(id));
         drugRepository.deleteById(id);
         return ResponseEntity.ok().body(drugRepository.findAll());
     }
