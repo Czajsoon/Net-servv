c84fdda716e70de0b5dc5c23099f0ddc718d3ddc
bkprog <73074013+bkprog@users.noreply.github.com>
2021-12-07 22:07:26 +0100

    changing-password window + validation
    ...

--- hospital-app/src/app/password-change/password-change.component.ts
+++ hospital-app/src/app/password-change/password-change.component.ts
@@ -1,5 +1,10 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {AuthService} from "../services/auth.service";
+
+
+import {FormBuilder,FormGroup,Validators} from "@angular/forms";
+import {MustMatch} from "../validators/MustMatch";
+import {MatSnackBar} from "@angular/material/snack-bar";
+
 
 @Component({
   selector: 'app-password-change',
@@ -8,27 +13,34 @@ import {AuthService} from "../services/auth.service";
 })
 export class PasswordChangeComponent implements OnInit {
   visibility = false;
-  @ViewChild('login') loginInput:ElementRef | any;
-  @ViewChild('password') passwordInput:ElementRef | any;
-  constructor(private auth:AuthService) { }
+  @ViewChild('repPassword1') newPasswordInput: ElementRef | any;
+  @ViewChild('repPassword2') repNewPasswordInput: ElementRef | any;
+  form: FormGroup | any;
+
+  constructor(private formBuilder: FormBuilder,
+              private toast: MatSnackBar) {
+
+  }
 
   ngOnInit(): void {
+  this.buildForm();
+  }
 
+  buildForm(){
+    this.form = this.formBuilder.group({
+      actualPassword:['',Validators.required],
+      newPassword:['',[Validators.required,Validators.minLength(8),Validators.maxLength(20)]],
+      repeatPassword:['',[Validators.required,Validators.minLength(8),Validators.maxLength(20)]]
+    },{
+      validator: MustMatch('newPassword','repeatPassword')
+    });
   }
 
   visibility_change(){
     this.visibility ? this.visibility = false : this.visibility = true;
   }
 
-  performLogin() {
-    this.auth.login(this.loginInput.nativeElement.value, this.passwordInput.nativeElement.value)
-      .then(data =>{
-
-      })
-      .catch(data=>{
-
-      })
-
-
-  }
+  changePassword(){
+      console.log("changepass");
+    }
 }
