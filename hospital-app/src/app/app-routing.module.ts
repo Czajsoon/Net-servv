40443a9ec9720626e26279e301666201b98e594b
DAWID598 <81323482+DAWID598@users.noreply.github.com>
2022-01-05 17:16:57 +0100

    viewdoctors V1

--- hospital-app/src/app/app-routing.module.ts
+++ hospital-app/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import {HomePageComponent} from "./home-page/home-page.component";
 import {TimetableComponent} from "./timetable/timetable.component";
 import {TimetableResolverService} from "./services/timetable-resolver.service";
 import {ReceptionistPanelComponent} from "./recepcionist-panel/receptionist-panel.component";
+import {ViewdoctorsComponent} from "./viewdoctors/viewdoctors.component";
 
 const routes: Routes = [
   {path:'',pathMatch: "full",component:HomePageComponent},
   {path:'login',component:LoginComponent},
   {path:'home',component:HomePageComponent},
   {path:'receptionist-panel',component:ReceptionistPanelComponent},
+  {path:'view-doctors',component:ViewdoctorsComponent},
   {path:'time-table',component:TimetableComponent,resolve:{
     time_table: TimetableResolverService
     }}
