02bd8fe3ed50e1c2dd8de00ffcfad3d10c90b9a2
MaTTi8N <mborcuchs090790@gmail.com>
2022-01-31 01:08:51 +0100

    poprawki
    poprawki wizualizacji

--- hospital-app/src/app/number-patient/number-patient.component.ts
+++ hospital-app/src/app/number-patient/number-patient.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./number-patient.component.scss']
 })
 export class NumberPatientComponent implements OnInit {
+// @ts-ignore
+  descInput: string
+  // @ts-ignore
+  descInput2: string
+  // @ts-ignore
+  descInput3: string
+  // @ts-ignore
+  descInput4: string
 
   constructor() { }
 
   ngOnInit(): void {
   }
+getUserInput(){
 
+  console.log(this.descInput);
+  console.log(this.descInput2);
+  console.log(this.descInput3);
+  console.log(this.descInput4);
+}
 }
